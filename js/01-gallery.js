f19b6dabf4f0614d4583410afc4f22a60cc8afe3
Olha-Suchok <suchok_olya@ukr.net>
2022-11-26 10:55:31 +0200

    Fixed markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,15 +6,15 @@ console.log(galleryImages);
 
 const makeImage = function () {
   return galleryItems
-    .map((image) => {
+    .map(({ preview, original, description }) => {
       return `
 <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="${image.preview}"
-      data-source="large-image.jpg"
-      alt="${image.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </div>
